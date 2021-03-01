688645e5462d93f982f74125e1481103ef176da7
ankur <ankurchaurasia002@gmail.com>
2021-03-02 00:37:07 +0530

    fotter complete

--- file/src/components/Footer.jsx
+++ file/src/components/Footer.jsx
@@ -4,6 +4,10 @@ import AccessTimeIcon from '@material-ui/icons/AccessTime';
 import EditLocationIcon from '@material-ui/icons/EditLocation';
 import EmailIcon from '@material-ui/icons/Email';
 import CopyrightIcon from '@material-ui/icons/Copyright';
+import WhatsAppIcon from '@material-ui/icons/WhatsApp';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import InstagramIcon from '@material-ui/icons/Instagram';
 import { NavLink } from 'react-router-dom';
 import logo from '../Image/logo.png';
 const Footer =()=>{
@@ -48,9 +52,17 @@ const Footer =()=>{
         </div>
 
         <div className="row endfoot" >
+        <div className="col-lg-6 col-12 order-0">
         <h2 className=" text-center" style={{fontSize:"15px"}}><CopyrightIcon className="icon"/>COPYRIGHT</h2>
         </div>
+        <div className="col-lg-6 col-12 order-1 d-flex justify-content-sm-center justify-content-lg-end gap-4 icons">
+            <a href="" className="whatsapp"><WhatsAppIcon></WhatsAppIcon></a>
+           <a href="https://www.facebook.com/Balancedbites10/" className="facebook"><FacebookIcon></FacebookIcon></a>
+            <a href="balancedbites10@gmail.com" className="mail"> <MailOutlineIcon></MailOutlineIcon></a>
+            <a href="https://www.instagram.com/balancedbites10/?r=nametag" className="insta"><InstagramIcon></InstagramIcon></a>
+             </div>
 
+        </div>
 
         </div>
         </>
