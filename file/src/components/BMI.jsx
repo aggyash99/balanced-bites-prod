c6a1b930afd666bafe1f7c3647ebe1ae7fcd29c2
name <git config --global user.name>
2021-04-20 19:07:30 +0530

    new

--- file/src/components/BMI.jsx
+++ file/src/components/BMI.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import {useState} from 'react'
-
+import image from '../Image/pics/bmi-1.jpg'
 const BMI = ()=>{
 
-    const [height, Height] = useState(0);
-    const [weight, Weight] = useState(0);
-    const [result, Result] = useState(0);
+    const [height, Height] = useState("");
+    const [weight, Weight] = useState("");
+    const [result, Result] = useState("");
 
     const set = (props) =>{
         Height(props.target.value);
@@ -20,19 +20,65 @@ const BMI = ()=>{
         const news = Atemp.toFixed(2);
         
         Result((news));
-        console.log(result);
+      //  console.log(result);
+      
     }
+
+    const reset = () =>{
+       
+            Height("");
+            Weight("");
+            Result("");
+      return ("");
+    }
+
     return (
-    <div className="container openss" >
+    <div className="container-fluid BMI p-5" style={{backgroundImage:`url(${(image)})`, backgroundPosition:"center",backgroundRepeat:"no-repeat",backgroundSize:"cover"}}>
         
-        <h1 className="text-capitalize text-center"> BMI</h1>
+ 
+        <div className="row" >
+        
+        <div className="col-lg-6 pt-5 mb-5">
 
-        <div> 
+        <h1 className="text-capitalize mb-4" style={{fontWeight:"700",}}> BMI CALCULATOR</h1>
 
+        <div className="BMI-start">
+       
+        <div className = "mb-2 d-flex p-3 input-BMI justify-content-around">
+        <input  type="number" placeholder="Your height (In cm)" onChange={set} value={height} style={{}}/>
+        <input placeholder="Your weight (In Kg)" type="number" onChange={setweight} value={weight} style={{}}/>
         </div>
-        <div className="row ">
+        <div className="mb-2 col-lg-12 d-flex justify-content-center">
+        <div className="btn btn-success " onClick={calculate}>calculate</div>
+         </div>  
 
-            <div className="row">
+         <div className="row p-3 input-MSG" >
+             <div className="d-flex flex-column gap-3">
+            <input value={result} placeholder="Your BMI"></input>
+            <input vlaue={""} placeholder="Your Status"></input>
+        </div>
+        </div>
+
+        <div className="col-lg-12 d-flex justify-content-center">
+        <div className="btn btn-success mb-2" onClick={reset}>RESET</div>
+         </div>
+        </div>
+        
+        </div>
+       
+        </div>
+        
+      
+
+    </div>
+);
+}
+
+
+export default BMI;
+
+{/*
+ <div className="row">
             <div className="col-lg-12 col-12 d-flex mb-3  justify-content-center" >
             <span>HEIGHT</span>
             <input  type="number" placeholder="In cm" onChange={set} style={{outline:"none",padding:"5px",textAlign:"center", width:"50%"}}/>
@@ -59,13 +105,5 @@ const BMI = ()=>{
                
             </div>
             </div>
-        </div>
 
-      
-
-    </div>
-);
-}
-
-
-export default BMI;
+*/ }
