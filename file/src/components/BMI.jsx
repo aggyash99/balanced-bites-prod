9611dc3196ad97649813d400a87a57ae908448bc
name <git config --global user.name>
2021-04-20 23:41:40 +0530

    new bmi

--- file/src/components/BMI.jsx
+++ file/src/components/BMI.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {useState} from 'react'
-import image from '../Image/pics/bmi-1.jpg'
+import image from '../Image/pics/bmi-2.jpg'
 const BMI = ()=>{
 
     const [height, Height] = useState("");
@@ -23,7 +23,6 @@ const BMI = ()=>{
       //  console.log(result);
       
     }
-
     const reset = () =>{
        
             Height("");
@@ -33,36 +32,62 @@ const BMI = ()=>{
     }
 
     return (
-    <div className="container-fluid BMI p-5" style={{backgroundImage:`url(${(image)})`, backgroundPosition:"center",backgroundRepeat:"no-repeat",backgroundSize:"cover"}}>
+    <div className="container-fluid BMI p-5" style={{backgroundImage:`url(${(image)})`}}>
         
  
-        <div className="row" >
+        <div className="row d-flex justify-content-end">
         
-        <div className="col-lg-6 pt-5 mb-5">
+        <div className="  d-flex justify-content-end  p-2 BMI-text"><span>BMI Calculator</span></div>
 
-        <h1 className="text-capitalize mb-4" style={{fontWeight:"700",}}> BMI CALCULATOR</h1>
+        <div className="col-lg-5 col-md-12 ">
 
-        <div className="BMI-start">
        
-        <div className = "mb-2 d-flex p-3 input-BMI justify-content-around">
-        <input  type="number" placeholder="Your height (In cm)" onChange={set} value={height} style={{}}/>
-        <input placeholder="Your weight (In Kg)" type="number" onChange={setweight} value={weight} style={{}}/>
+        <form className="BMI-start">
+         
+        <div className="row">
+        <div className="col-lg-6 col-md-6 col-12 input-BMI d-flex justify-content-center">
+        <input placeholder="Your weight (In Kg)" type="number" onChange={setweight} value={weight}  />
         </div>
-        <div className="mb-2 col-lg-12 d-flex justify-content-center">
-        <div className="btn btn-success " onClick={calculate}>calculate</div>
-         </div>  
-
-         <div className="row p-3 input-MSG" >
-             <div className="d-flex flex-column gap-3">
-            <input value={result} placeholder="Your BMI"></input>
-            <input vlaue={""} placeholder="Your Status"></input>
+        <div className="col-lg-6 col-md-6  col-12 input-BMI d-flex justify-content-center">
+        <input  type="number" placeholder="YOUR HEIGHT In cm" onChange={set} />
         </div>
         </div>
-
+        <div className="row">
         <div className="col-lg-12 d-flex justify-content-center">
-        <div className="btn btn-success mb-2" onClick={reset}>RESET</div>
+        <div className="btn success " onClick={calculate}>Calculate BMI</div>
+        </div>
+        </div>
+        <div className="row">
+        <div className="col-lg-12 pt-4 d-flex flex-column input-BMI">
+        <input value={result} placeholder="Your BMI"></input>
+        <input vlaue={""} placeholder="Your Status"></input>
+        </div>
+        </div>
+        <div className="row">
+            <div className="col-lg-12 d-flex justify-content-center">
+        <div className="btn success" onClick={reset}>Reset</div>
+        </div>
+        </div>
+
+       {/*<input  type="number" placeholder="Your height (In cm)" onChange={set} value={height}  />
+    */}
+
+
+        {/*<div className="mb-2 mt-2 col-lg-12 d-flex justify-content-center">
+        </div>  
+
+         <div className="row p-4 input-MSG" >
+             <div className="d-flex flex-column gap-3">
          </div>
         </div>
+*/}
+        {/*<div className="col-lg-12 d-flex justify-content-center">
+         </div>
+         */}
+
+
+
+        </form>
         
         </div>
        
@@ -81,8 +106,7 @@ export default BMI;
  <div className="row">
             <div className="col-lg-12 col-12 d-flex mb-3  justify-content-center" >
             <span>HEIGHT</span>
-            <input  type="number" placeholder="In cm" onChange={set} style={{outline:"none",padding:"5px",textAlign:"center", width:"50%"}}/>
-            </div>
+             </div>
             </div>
             <div className="row">
             <div className="col-lg-12 col-12 mb-3 d-flex justify-content-center">
