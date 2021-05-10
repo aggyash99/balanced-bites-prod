1076da47bec2c14f0dc543076be8c5f2b15c0437
Naman <sumannaman7@gmail.com>
2021-05-10 23:17:31 +0530

    naman7

--- file/src/components/page/Modal.jsx
+++ file/src/components/page/Modal.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { CSSTransition } from "react-transition-group";
 import "./modal.css";
 import CloseIcon from '@material-ui/icons/Close'; 
-
+import $ from 'jquery';
 const Modal = props => {
   const closeOnEscapeKeyDown = e => {
     if ((e.charCode || e.keyCode) === 27) {
@@ -18,30 +18,44 @@ const Modal = props => {
     };
   }, []);
 
+
+  function vidplay()
+  {
+    $("#video").addClass("opacitymodal");
+    document.getElementById("video").play();
+  }
+
   return ReactDOM.createPortal(
     <CSSTransition
       in={props.show}
       unmountOnExit
-      timeout={{ enter: 0, exit: 300 }}
+      timeout={{enter: 0, exit: 0 }}
     >
         <>
    
         <div className="modal" onClick={props.onClose}>
                     <div className="modal-content" onClick={e => e.stopPropagation()}>
-                        <div className="modal-header">
-                            <h4 className="modalheader">Thank You</h4>
-                            <button className="btn btn-outline btnmodalclose" onClick={props.onClose} >
-                            <CloseIcon  />
-                            </button>
-                            </div>
                     <div className="modal-body ">
+                    <div className="row ">
+                          
+                          <div className="col-12 btnmodalalign">
+                           
+                            <button className="btn btn-outline btnmodalclose" onClick={props.onClose}  >
+                            <CloseIcon  />
+                            </button></div></div>
+                        <div className="row ">
+                          
+                        <div className="col-12 justifycontentcenter" >
+                        
+                          <video className="vidmodal" height="200" width="200" id="video" autoPlay>
+                            <source  src="https://media.tenor.com/videos/b1193bfdeb305b8e34dc975177590f53/mp4#t=0.1" type="video/mp4"/>
+                          </video>
+                          </div>
+                          </div>
                         <div className="row">
                             <div className="col-12 justifycontentcenter">We will contact you as soon as possible </div>
                         </div>
-                        <div className="row">
-                            <div className="col-12 justifycontentcenter" >
-                    <button onClick={props.onClose} className="btn btn-outline btnmodal">Okay!</button>
-                    </div></div></div>
+                   </div>
                 </div>
                 </div>
        
