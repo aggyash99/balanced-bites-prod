0e39352b4b37b58fff71cb420c7da59532dc19ca
Naman <sumannaman7@gmail.com>
2021-05-28 10:04:59 +0530

    minorphotosadded

--- file/src/components/page/Modal.jsx
+++ file/src/components/page/Modal.jsx
@@ -11,7 +11,14 @@ const Modal = props => {
 /* <video className="vidmodal" height="200" width="200" id="video" autoPlay>
                             <source  className="vidmodal2" src="https://media.tenor.com/videos/b1193bfdeb305b8e34dc975177590f53/mp4#t=0.1" type="video/mp4"/>
                           </video>
-                         */
+  
+  style="--i:1"
+style="--i:2"
+style="--i:3"
+style="--i:4"
+style="--i:5"
+style="--i:6"
+style="--i:7"                        */
 
   
   var shortenedname;
@@ -36,14 +43,27 @@ const Modal = props => {
       timeout={{enter: 0, exit: 0 }}
     >
         <>
-   
         <div className="modal" onClick={props.onClose}>
+          
+        <span className="underlinebordermodal "></span>
                     <div className="modal-content" onClick={e => e.stopPropagation()}>
                     <div className="modal-body ">
                     <div className="row ">
+                    <div className="col-12 justifycontentcenter  " >
+                    <div className="resituateripplemodal">
+                        <span className="RippleModal" style={{animationDelay:"0.5s"}}></span>
+                        <span className="RippleModal" style={{animationDelay:"1s"}}></span>
+                        <span className="RippleModal" style={{animationDelay:"1.5s"}}></span>
+                        <span className="RippleModal" style={{animationDelay:"2s"}}></span>
+                        <span className="RippleModal" style={{animationDelay:"2.5s"}}></span>
+                        <span className="RippleModal" style={{animationDelay:"3s"}}></span>
+                        <span className="RippleModal" style={{animationDelay:"3.5s"}}></span>
+                        </div>
+                        </div></div>
+                    <div className="row ">
                           
                           <div className="col-12 btnmodalalign">
-                           
+                          
                             <button className="btn btn-outline btnmodalclose"  >
                             <Link className="" to={{pathname:'/'}} target="_top" >
                             <CloseIcon style={{fontSize:"25px"}} /></Link>
@@ -51,10 +71,10 @@ const Modal = props => {
                         
                         <div className="row ">
                          
-                        <div className="col-12 justifycontentcenter " >
-                        <video className="" height="200" width="200" id="video" autoPlay>
-                            <source  className="vidmodal2" src="https://media.tenor.com/videos/b1193bfdeb305b8e34dc975177590f53/mp4#t=0.1" type="video/mp4"/>
-                          </video>   </div>
+                        <div className="col-12 justifycontentcenter heightincmodal " >
+                         
+                        <img src={Tick} height="175" width="175" className="vidmodal"></img> </div>
+                   
                           </div>
                         <div className="row">
                             <div className="col-12 justifycontentcenter" style={{paddingBottom:"0px"}}>
@@ -66,7 +86,7 @@ const Modal = props => {
                    </div>
                 </div>
                 </div>
-       
+                
         </>
         </CSSTransition>,
     document.getElementById("root")
