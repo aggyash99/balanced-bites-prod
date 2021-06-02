964947dbcf85a38c98e17cdd166008e0b04244d5
Ankur <ankurchaurasia002@gmail.com>
2021-06-02 22:44:26 +0530

    new

--- file/src/components/page/Home.jsx
+++ file/src/components/page/Home.jsx
@@ -1,7 +1,9 @@
 import React, {useState} from 'react';
-import imgs from '../../Image/pics/home.jpg';
-import Photo from '../photo'
-import img from '../../Image/pics/whatsapp1.svg';
+import imgs from '../../Image/owner/bg.jpg';
+import Photo from '../photo';
+import owner from '../../Image/owner/owner2.png'
+import img from '../../Image/images/whatsapp-1.png';
+import circle from '../../Image/images/circle.svg';
 import CarouselH from '../CarouselH'; 
 import BMI from '../BMI';
 import Reviewcard from '../Newcards';
@@ -13,22 +15,36 @@ function Home() {
   return (
     <>
     <section className = "header" id="headerindex">
-    
-    <a href="https://wa.link/k2f045"><span className="screen-bar"><img src={img}></img></span></a>
-
+    <div  className="screen-bar">
+      <img className="animates" src={circle}></img>
+      <img className="animates1" src={circle}></img>
+       <a href="https://wa.link/k2f045"><span><img src={img}></img></span></a>
+    </div>
       <div className="container-fluid position-relative overflow-hidden" >
         <div className="row home-img" >
-          <div className="col-lg-12 d-flex justify-content-around mx-auto p-0 para" >
-              <img  style={{width:"100%"}}  src={imgs} alt ="sorry"></img>
+          <div className="col-lg-12 d-flex justify-content-around mx-auto position-relative p-0 para" >
+              <img  style={{width:"100%",opacity:"0.4"}}  src={imgs} alt ="sorry"></img>
             
+              <img className="owner1" src={owner}></img>
+               
           </div>
           <div>
           <p >Exersizes is not sufficient for your body, It changes your mind,attitude.
-             <Link to='/contact'>Consult Today  </Link>
+            <div className="add-new  mt-3">
+              Muskan Bansal
+              <div>
+                Nuetritionist and diabeties Educator  
+              </div>
+              </div>
+             <div className="" style={{marginLeft:"30px",marginTop:"20px"}}> 
+             <Link to='/contact'>Contact Us </Link>
+             {/* <Link to='/contact'>Programs </Link> */}
+             </div>
           </p>
           <h6>Healthy Mood, Healthy Mind
              <Link to='/contact'> Consult Today  </Link>
           </h6>
+          
           </div>
           {/* <div className="home-img-fil">
           </div> */}
