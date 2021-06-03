74799127e54325764a07d5f9b89d627a2d621c06
Ankur <ankurchaurasia002@gmail.com>
2021-06-03 17:20:31 +0530

    new

--- file/src/components/Footer.jsx
+++ file/src/components/Footer.jsx
@@ -15,7 +15,7 @@ const Footer =()=>{
     return (
         <>
         <div className="End-footer" >
-            <div className="container-fluid" style={{background : "black"}}>
+            <div className="container-fluid" style={{background : "#1c1c1c"}}>
             <div className="row">
                 <IMGcolumn className="d-flex justify-content-center">
                 <Upper>
@@ -31,18 +31,50 @@ const Footer =()=>{
                 </IMGcolumn> 
             </div>
 
-            <div className="row">
-            <div className="col-lg-3 col-md-6 col-12">
-                <span>Get In Touch</span>
-            </div>  
+            <div className="row mt-2">
+            <Col className="col-lg-4 col-md-6 col-12">
+                <span>About Us</span>
+                <FooterLink className="foot1">
+                <ul>
+                <li>Muskan Bansal is a young & aspiring dietitian, nutritionist and Diabetes Educator from Delhi, India. She is enthusiastically working in the field of weight management and therapeutic nutrition. She believes in a natural way of achieving a healthy lifestyle...</li>
+                <li className="newlink"><LINKS href="/about" >Read More</LINKS></li>
+                </ul> 
+                </FooterLink>
+            </Col>  
 
-            <div className="col-lg-6 col-md-6 col-12">
-                <span>Programs</span>
-            </div>  
+            <Col className="col-lg-4 col-md-6 col-12">
+                <span>Our Programs</span>
+                <FooterLink>
+                <ul>
+                <li>Weight Loss</li>
+                <li>Weight Gain</li>
+                <li>Pregnancy</li>
+                <li>Child Nutrition</li>
+                
+                </ul>
+                <ul>
+                <li>PCOS</li>
+                <li>Detox Diet</li>
+                <li>Diabetes</li>
+                <li>Thyroid</li>
+                
+                <li>Hair and Skin care</li>
+                </ul>
+                </FooterLink>
+            </Col>  
 
-            <div className="col-lg-3 col-md-6 col-12">
-                <span></span>
-            </div>  
+            <Col className="col-lg-4 col-md-6 col-12">
+            <span>Get In Touch</span>
+            <FooterLink className="foot3">
+            <ul>
+            <div><CallIcon className="icon"/><li>Call : +91 8700326422</li></div>
+            <div><AccessTimeIcon className="icon"/> <li>Timing : Mon-Sat 10:00Am - 7:00 PM</li></div>
+            <div> <EditLocationIcon className="icon" /> <li>Address : Hno. 288-89 G-26 Sec-3 ROHINI , DELHI</li></div>
+            <div><EmailIcon className="icon"/> <li>EMAIL : balancedbites10@gmail.com</li></div>
+            </ul>
+            </FooterLink>
+            
+            </Col>  
 
             </div>
 
@@ -56,6 +88,67 @@ const Footer =()=>{
 };
 
 export default Footer;
+const FooterLink = Styled.div`
+
+ul{
+    list-style : none;
+    text-align : center;
+    margin : 0;
+    padding: 20px 0;
+    font-size: 17px;
+
+}
+li{
+    text-align : center;
+    padding : 10px;    
+}
+
+display : flex; 
+justify-content : space-around;
+align-items : center;
+
+`
+const Col = Styled.div`
+color : white;
+font-size : 20px;
+font-family : poppins;
+padding : 5px;
+text-align : center;
+span {
+    position : relative;
+    &:before{
+        position : absolute;
+        content : "";
+        width : 30px;
+        height : 3px;
+        background : #297300;
+        bottom : -10px;
+    }
+}
+.foot1{
+    padding : 20px;
+    li{
+        text-align : left;
+        margin-left : 40px;
+        @media (max-width : 400px)
+        {
+            margin : 0;
+        }
+    }
+}
+.foot3{
+    padding : 20px;
+    text-align : left;
+    li{
+        text-align : left; 
+    }
+    div{
+        display : flex;
+        align-items :center;
+    }
+}
+
+`
  
 const Img = Styled.img`
 object-fit: contain;
@@ -67,6 +160,7 @@ margin : 10px;
 color : white;
 }
 display : flex;
+align-items : center;
 justify-content : space-around;
 `
 const LINKS = Styled.a`
