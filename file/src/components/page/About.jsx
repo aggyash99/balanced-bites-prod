69db5a0bd019734ffe79e3ad9c31ce1771911634
Naman <sumannaman7@gmail.com>
2021-06-19 10:30:58 +0530

    naman15

--- file/src/components/page/About.jsx
+++ file/src/components/page/About.jsx
@@ -1,5 +1,17 @@
 import React,{ Component, lazy, Suspense } from 'react';
 import AboutRender from './AboutRender'
+import Logo from '../../Image/logo.png';
+ 
+
+import imgs from '../../Image/owner/bg.jpg';
+ 
+import owner from '../../Image/owner/owner2.png'
+import img2 from '../../Image/services/doodle.jpg'; 
+import './about.css'; 
+ 
+import Reviewcard from '../Newcards2';
+import { Link } from 'react-router-dom';
+import SmallerIMG from '../../Image/images/About Smaller Img.jpeg';
 
 class About extends Component{
  
